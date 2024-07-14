88ab9cde2ff7009de3732f66888c0a0f3336c30e
Дмитрий <dima.gladkikh.2016@mail.ru>
2024-07-15 06:57:35 +0800

    Исправлено отображение изображений

--- catalog/admin.py
+++ catalog/admin.py
@@ -8,8 +8,9 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
 
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
-    list_filter = ('category',)
-    search_fields = ('name', 'description')
+admin.site.register(Product)
+# @admin.register(Product)
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'name', 'price', 'category')
+#     list_filter = ('category',)
+#     search_fields = ('name', 'description')
